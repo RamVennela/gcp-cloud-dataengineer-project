ad338169f04a3e3ac03d60ca6eae5fd574dbe940
Ram Vennela <ramvennela150520@gmail.com>
2025-01-22 12:41:44 +0000

    pyspark code

---
+++ pyspark_scripts/nyc-rest-result_bronze.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+import pyspark
+from pyspark.sql import SparkSession
+
+# In[10]:
+spark = SparkSession.builder.appName("write to GCS").getOrCreate()
+
+# In[14]:
+input_path='gs://resturent_nyc_dataset/NYC Restaurant Inspection/NYC_Restaurant_Inspection_Results_20250121.csv'
+df = spark.read.csv(input_path,inferSchema=True)
+
+# In[21]:
+df.printSchema()
+df.count()
+
+# In[33]:
+bucket_name = "resturent_nyc_dataset"
+file_path = f"gs://{bucket_name}/NYC Restaurant Inspection/bronze_data"
+df.write.mode("overwrite").csv(file_path)
+
+spark.stop()
---
+++ pyspark_scripts/write_to_bigquery.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+# In[2]:
+spark = SparkSession.builder.appName("Write to BigQuery").getOrCreate()
+
+# In[3]:
+input_path='gs://resturent_nyc_dataset/NYC Restaurant Inspection/NYC_Restaurant_Inspection_Results_20250121.csv'
+df = spark.read.csv(input_path,header=True,inferSchema=True)
+
+# In[4]:
+dropdup = df.dropDuplicates()
+dropdup.count()
+
+# In[5]:
+dropna = dropdup.dropna(subset="CAMIS")
+dropna.count()
+
+# In[6]:
+df_cleaned = dropna.select(
+    [F.col(column).alias(column.replace(" ", "_").replace(":", "_")) for column in dropna.columns]
+)
+print("Cleaned Column Names:", df_cleaned.columns)
+
+# In[7]:
+df_uppercase = df_cleaned.toDF(*[col.upper() for col in df.columns])
+
+# In[8]:
+print(df_uppercase)
+
+# In[9]:
+df_cleaned.write.format("bigquery").option("table","dotted-banner-448417-n1.nyc_rest_results.nyc_rest_results_insp").option("temporaryGcsBucket", "resturent_nyc_dataset").mode("overwrite").save()
+
+# In[10]:
+spark.stop()
